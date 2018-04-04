b5e206e13df746812c6e76370d5bf299f63cf47c
Krasimir Georgiev <krasi2405@gmail.com>
2018-04-05 01:43:27 +0300

    Add script for health and manabar

---
+++ Assets/Scripts/UIBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBar : MonoBehaviour {
+
+    [SerializeField]
+    private float maxAmount;
+
+    [SerializeField]
+    private float currentAmount;
+
+    private Image image;
+
+	void Start () {
+        image = GetComponent<Image>();
+        image.type = Image.Type.Filled;
+        image.fillMethod = Image.FillMethod.Horizontal;
+        image.fillOrigin = 0;
+    }
+
+
+	void Update () {
+        ChangeUIBar();
+	}
+    
+
+    public void ChangeUIBar()
+    {
+        image.fillAmount = currentAmount / maxAmount;
+    }
+}
