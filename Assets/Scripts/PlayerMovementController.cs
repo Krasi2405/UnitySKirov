1ac5a09eb9c1388e9947b7a6c14ddaf67468d285
MIsho <shimonchick@gmail.com>
2018-04-10 18:03:47 +0200

    changed player movement controller

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour {
 
-    private bool isWalkingUp;
-    private bool isWalkingDown;
-    private bool isWalkingLeft;
-    private bool isWalkingRight;
+
+    [SerializeField] private int movementSpeed = 1;
     [SerializeField] private Sprite playerLeftSprite;
     [SerializeField] private Sprite playerRightSprite;
     [SerializeField] private Sprite playerUpSprite;
@@ -16,6 +14,9 @@ public class PlayerMovementController : MonoBehaviour {
     private Sprite directionSprite;
     private SpriteRenderer spriteRenderer;
     private Animator animator;
+
+
+
     void Start () {
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
@@ -25,59 +26,58 @@ public class PlayerMovementController : MonoBehaviour {
 	void Update () {
         ProcessInput();
 	}
+    private void ProcessInput() {
+        float xOffset = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        float yOffset = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        if (xOffset > 0) // right
+        {
 
-    private void ProcessInput()
-    {
-        if (Input.GetKey(KeyCode.A))
-        {
-            spriteRenderer.sprite = playerLeftSprite;
-            animator.SetBool("isWalkingLeft", true);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            spriteRenderer.sprite = playerRightSprite;
-            animator.SetBool("isWalkingRight", true);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            spriteRenderer.sprite = playerDownSprite;
-            animator.SetBool("isWalkingDown", true);
-        }
-        else if (Input.GetKey(KeyCode.W))
-        {
-            spriteRenderer.sprite = playerUpSprite;
-            animator.SetBool("isWalkingUp", true);
-        }
-
-
-
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            directionSprite = playerUpSprite;
-            animator.SetBool("isWalkingUp", false);
-        }
-        if (Input.GetKeyUp(KeyCode.S))
-        {
-            directionSprite = playerDownSprite;
-            animator.SetBool("isWalkingDown", false);
-        }
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            directionSprite = playerLeftSprite;
-            animator.SetBool("isWalkingLeft", false);
-        }
-        if (Input.GetKeyUp(KeyCode.D))
-        {
+            if (!animator.GetBool("isWalkingRight")) animator.SetBool("isWalkingRight", true);
             directionSprite = playerRightSprite;
+            SetDirectionSprite();
+
+        }
+        else if (xOffset < 0) // left
+        {
+
+            if (!animator.GetBool("isWalkingLeft")) animator.SetBool("isWalkingLeft", true);
+            directionSprite = playerLeftSprite;
+            SetDirectionSprite();
+
+
+        }
+        else if (yOffset > 0) // up
+        {
+
+
+            if (!animator.GetBool("isWalkingUp")) animator.SetBool("isWalkingUp", true);
+            directionSprite = playerUpSprite;
+            SetDirectionSprite();
+
+        }
+        else if (yOffset < 0) // down
+        {
+            if (!animator.GetBool("isWalkingDown")) animator.SetBool("isWalkingDown", true);
+            directionSprite = playerDownSprite;
+            SetDirectionSprite();
+
+        }
+        else {
+            animator.SetBool("isWalkingDown", false);
+            animator.SetBool("isWalkingUp", false);
+            animator.SetBool("isWalkingLeft", false);
             animator.SetBool("isWalkingRight", false);
         }
-        
+        float Xposition = transform.position.x + xOffset;
+        float Yposittion = transform.position.y + yOffset;
+        transform.position = new Vector2(Xposition, Yposittion);
 
     }
 
 
+
     public void SetDirectionSprite()
     {
-        GetComponent<SpriteRenderer>().sprite = directionSprite;
+        spriteRenderer.sprite = directionSprite;
     }
 }
