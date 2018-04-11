62c365469ada0f5307e2efa81840086eca32fa78
Krasimir Georgiev <krasi2405@gmail.com>
2018-04-11 23:30:23 +0300

    follow camera

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    [SerializeField] private Transform player; // TODO: Change to player script when added.
+
+    private void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position, 
+            new Vector3(player.position.x, player.position.y, transform.position.z), 
+            0.2f);
+    }
+}
