c437758e38e17bbbf4cf4cccc7954e23bc47dc23
Krasimir Georgiev <krasi2405@gmail.com>
2018-04-13 22:22:00 +0300

    Fix errors

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -5,15 +5,14 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour {
 
     [SerializeField] private Transform player; // TODO: Change to player script when added.
-    [SerializeField] private float delay;
-    private Vector3 velocity = Vector3.zero;
+    [SerializeField] private float followCoefficient = 0.15f;
+
+
     private void LateUpdate()
     {
-        transform.position = Vector3.SmoothDamp(
-            transform.position,
-            new Vector3(player.position.x, player.position.y, transform.position.z),
-            ref velocity,
-            delay
-            );
+        transform.position = Vector3.Lerp(
+            transform.position, 
+            new Vector3(player.position.x, player.position.y, transform.position.z), 
+            followCoefficient);
     }
 }
