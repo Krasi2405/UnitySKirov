d76a5abce2e3c226b78b4ff98f7fbd50576ca5ac
Krasimir Georgiev <krasi2405@gmail.com>
2018-04-15 22:59:30 +0300

    Connect healthbar to player hp

--- Assets/Scripts/Healthbar.cs
+++ Assets/Scripts/Healthbar.cs
@@ -3,5 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Healthbar : UIBar {
-    
+
+
+    HitController hitController;
+
+    // TODO: call everything from HitController!;
+
+    private void Start()
+    {
+        hitController = FindObjectOfType<HitController>();
+        SetMaxAmount(hitController.hp);
+    }
+
+    private void Update()
+    {
+        SetCurrentAmount(hitController.hp);
+        ChangeUIBar();
+    }
 }
--- Assets/Scripts/UIBar.cs
+++ Assets/Scripts/UIBar.cs
@@ -7,27 +7,35 @@ public class UIBar : MonoBehaviour {
 
     [SerializeField]
     private float maxAmount;
-
     [SerializeField]
     private float currentAmount;
 
-    private Image image;
+    [SerializeField] private Image image;
 
 	void Start () {
-        image = GetComponent<Image>();
+        /*
         image.type = Image.Type.Filled;
         image.fillMethod = Image.FillMethod.Horizontal;
         image.fillOrigin = 0;
+        */ // TODO: Da go napravq da raboti :)
     }
 
-
-	void Update () {
-        ChangeUIBar();
-	}
     
+    public void SetMaxAmount(float amount)
+    {
+        maxAmount = amount;
+    } 
+
+    
+    public void SetCurrentAmount(float amount)
+    {
+        currentAmount = amount;
+    }
+
 
     public void ChangeUIBar()
     {
         image.fillAmount = currentAmount / maxAmount;
+        Debug.Log("change uibar to " + currentAmount / maxAmount);
     }
 }
