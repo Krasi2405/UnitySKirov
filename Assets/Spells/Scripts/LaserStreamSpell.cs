74f8d36ee04fa50dc625c31330e0998cf4109b9d
Krasimir Georgiev <krasi2405@gmail.com>
2018-05-07 01:36:07 +0300

    WIP spell kato laser.

---
+++ Assets/Spells/Scripts/LaserStreamSpell.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserStreamSpell : SpellBehaviour {
+
+    [SerializeField] GameObject laserPrefab;
+
+
+    protected override void TurnOnBehaviour()
+    {
+        Instantiate(laserPrefab);
+    }
+
+    protected override bool CastConditionsFulfilled()
+    {
+        return true;
+    }
+}
