335186c560c1ea8c116b4a7e2462e0c45379c35c
MIsho <shimonchick@gmail.com>
2018-05-07 13:26:52 +0200

    added Universal sprites to resources

---
+++ Assets/CustomizationManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomizationManager : MonoBehaviour {
+
+    Sprite[] bodylayer;
+    Sprite[] hairlayer;
+    int currentBody, currentHair;
+    string sex = "male";
+    Image representation;
+
+
+    void Start() {
+        representation = GameObject.Find("representation").GetComponent<Image>();
+        currentBody = 0;
+        currentHair = 0;
+        bodylayer = Resources.LoadAll<Sprite>("Universal_Sprites/body/" + sex);
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    public void ChangeSex()
+    {
+        if(sex == "male")
+        {
+            sex = "female";
+        }
+        else
+        {
+            sex = "male";
+        }
+    }
+}
+
